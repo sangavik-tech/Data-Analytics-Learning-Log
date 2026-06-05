b2b5ab4620c8d5a3ec101c76322a40217b8a0aa2
Sangavi K <sangavisudha12@gmail.com>
2026-06-05 16:41:33 +0530

    Update Day-1-Basic-Select.sql

--- Day-1-Basic-Select.sql
+++ Day-1-Basic-Select.sql
@@ -1,3 +1,14 @@
+SQL
+-- CONCEPT: Basic SELECT Statement
+-- DATE: 2026-06-05
+-- CHALLENGE: Ran my first query on the Customers and Products tables.
+
+SELECT * FROM Customers;
+
+-- NOTE: The '*' means "select all columns". 
+-- Next time I want to learn how to pick only specific columns.
+
+SQL
 -- CONCEPT: SELECT Statement
 -- DATE: 2026-06-05
 -- CHALLENGE: Learned how to pull specific columns from the 'Customers' table.
